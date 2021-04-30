e62c8a0ce6fdb5669f71e653892416cd1abe7bf1
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2021-04-30 19:01:28 +0200

    first commit

---
+++ src/test/java/com/ignacio/twitter/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacio.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
